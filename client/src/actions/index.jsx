fe7e9b2a32eceb6095a56b99346c0c23a002bcd9
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-15 12:12:54 +0100

    Stripe Payment

--- client/src/actions/index.jsx
+++ client/src/actions/index.jsx
@@ -3,8 +3,18 @@ import { FETCH_USER } from './types';
 
 export const fetchUser = () => async dispatch => {
   const { data } = await axios.get('/api/current_user');
+  console.log(data)
+
   dispatch({
     type: FETCH_USER,
     payload: data
+  })
+}
+
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  dispatch({
+    type: FETCH_USER,
+    payload: res.data
   })
 }
