214ee6fd59449dcaed1409a38f973da4c3374031
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-21 10:09:04 +0100

    Redux form setup

---
+++ client/src/components/surveys/SurveyNew.jsx
@@ -0,0 +1,14 @@
+// Survey New shows Survey Form and Suvey Review
+import React, { Component } from 'react';
+
+import SurveyForm from './SurveyForm';
+
+export default class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    )
+  }
+}
