c498ab2747a5e8074257804533d045e1a7dfd211
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-01-22 10:07:01 +0100

    Form validation

--- client/src/components/surveys/SurveyForm.jsx
+++ client/src/components/surveys/SurveyForm.jsx
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+  {label: 'Survey Title', name:'title'},
+  {label: 'Subject line', name: 'subject'},
+  {label: 'Email body', name: 'body'},
+  {label: 'Recipient List', name: 'emails'}
+]
 class SurveyForm extends Component {
+
+  renderFields() {
+    return FIELDS.map(({label, name}) => {
+      return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+    })
+  }
+
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field 
-            type="text"
-            name="surveyTitle"
-            component = "input"
-          />
-          <button type="submit">Submit</button>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     )
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title){
+    errors.title = 'You must provide a title';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
